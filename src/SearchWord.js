f094f0ccc4e1aa738cfc53e33de0d3bdfaad805a
Hejaz Nawasser <hejazn@gmail.com>
2021-09-26 00:39:12 +0330

    add API integration

--- src/SearchWord.js
+++ src/SearchWord.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState} from 'react';
 import './SearchWord.css';
+import axios from 'axios';
 
 export default function SearchWord(){
+    const [word, setWord] = useState(null);
+
     
     function handleSubmit(event){
         event.preventDefault();
-        alert(event.target.value);
+
     }
+
+    function changeWord(event){
+        event.preventDefault();
+        setWord(event.target.value);
+    }
+
+    function handleResult(response){
+        console.log(response.data);
+    }
+    
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    axios.get(apiUrl).then(handleResult)
     return (
         <div className='SearchWord'>
             <form onSubmit={handleSubmit}>
-                <input type="search" className="form-input w-50 me-2" placeholder="Search a word..."/> 
-                <input  type='Submit' value='search' className="btn btn-warning"/>
+                <input type="search" className="form-control w-50" placeholder="Search a word..." onChange={changeWord}/> 
+    
             </form>
         </div> 
         
